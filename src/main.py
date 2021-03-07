5241ea8fdacdbc0b45cb07e9cee8041f8a36e5db
Tommy Huang <tommyhuang999@gmail.com>
2021-03-07 00:26:03 -0500

    Add engine file

--- src/main.py
+++ src/main.py
@@ -1,40 +1,56 @@
 import chess
 import sys
+from superior_engine import SuperiorEngine
+
+def invert_board(board : str):
+    return "\n".join(board.splitlines()[::-1])
 
 # setup
 board = chess.Board()
+engine = SuperiorEngine()
 
 if len(sys.argv) > 1 and sys.argv[1] == "black":
-    playerToMove = False
-    playerColour = "black"
+    player_to_move = False
+    player_colour = "black"
 else:
-    playerToMove = True
-    playerColour = "white"
-    
+    player_to_move = True
+    player_colour = "white"
+
+# time limit for the engine in seconds
+engine_time_limit = 2
+if len(sys.argv) > 2:
+    engine_time_limit = float(sys.argv[2])
 
 while not board.is_game_over(claim_draw=True):
-    # since we use darktheme, gotta invert colours when playing as white
+    # since we use darktheme, gotta invert colours when drawing board
     print("----------------")
-    print(board.unicode(invert_color = playerColour == "white"))
+    if (player_colour == 'black'):
+        print(board.unicode(invert_color=True)[::-1])
+    else:
+        print(board.unicode(invert_color=True))
 
-    while playerToMove:
-        nextMove = input()
-        if nextMove == "u":
-            try:
-                board.pop()
-                playerToMove = False
-            except ValueError:
-                print("cannot takeback further")
-        else:
-            try:
-                board.push_san(nextMove)
-                playerToMove = False
-            except ValueError:
-                print("invalid move you suck")
-    
-    # engine moves
 
-    playerToMove = True
+    if (player_to_move):
+        while player_to_move:
+            next_move = input()
+            if next_move == "u":
+                try:
+                    board.pop()
+                    board.pop()
+                    print("----------------")
+                    print(board.unicode(invert_color = player_colour == "white"))
+                except IndexError:
+                    print("cannot takeback further")
+            else:
+                try:
+                    board.push_san(next_move)
+                    player_to_move = False
+                except ValueError:
+                    print("invalid move you suck")
+    else:
+        result = engine.play(board, time_limit=engine_time_limit)
+        board.push(result)
+        player_to_move = True
 
 print("GAME OVER!!!")
 print(board.result(claim_draw=True))
--- src/superior_engine.py
+++ src/superior_engine.py
@@ -1,3 +1,17 @@
+import time
+
 class SuperiorEngine:
     def __init__(self):
         pass
+
+    def play(self, board, time_limit):
+        # get copy of move stack
+
+        start_time = time.time()
+
+        while time.time() - start_time < time_limit:
+            legalMoves = list(board.legal_moves)
+            print(legalMoves[0])
+            return legalMoves[0]
+
+        # reset move stack
