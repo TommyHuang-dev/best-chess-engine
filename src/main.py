8b48348055e4cb96d13914b9ad7929ad6e468f93
Tommy Huang <tommyhuang999@gmail.com>
2021-03-06 23:40:49 -0500

    added support for inverting the board

--- src/main.py
+++ src/main.py
@@ -1,20 +1,40 @@
 import chess
-import chess.svg
+import sys
 
-class SuperiorEngine:
-    def __init__(self):
-        pass
-
+# setup
 board = chess.Board()
 
-while not board.is_game_over(claim_draw=True):
-    print(board.unicode())
+if len(sys.argv) > 1 and sys.argv[1] == "black":
+    playerToMove = False
+    playerColour = "black"
+else:
+    playerToMove = True
+    playerColour = "white"
+    
 
-    nextMove = input()
-    if nextMove == 'u':
-        board.pop()
-    else:
-        board.push(chess.Move.from_uci(nextMove))
+while not board.is_game_over(claim_draw=True):
+    # since we use darktheme, gotta invert colours when playing as white
+    print("----------------")
+    print(board.unicode(invert_color = playerColour == "white"))
+
+    while playerToMove:
+        nextMove = input()
+        if nextMove == "u":
+            try:
+                board.pop()
+                playerToMove = False
+            except ValueError:
+                print("cannot takeback further")
+        else:
+            try:
+                board.push_san(nextMove)
+                playerToMove = False
+            except ValueError:
+                print("invalid move you suck")
+    
+    # engine moves
+
+    playerToMove = True
 
 print("GAME OVER!!!")
 print(board.result(claim_draw=True))
---
+++ src/superior_engine.py
@@ -0,0 +1,3 @@
+class SuperiorEngine:
+    def __init__(self):
+        pass
